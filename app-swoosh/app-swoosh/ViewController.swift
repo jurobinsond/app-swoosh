8c3043a59b8bf29411d7e5d1f823ef706b71f5b9
Justin <justin@tlgphotraphymbp.home>
2017-12-06 16:21:23 -0500

    constraints

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Justin on 11/15/17.
+//  Copyright © 2017 Justin Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
